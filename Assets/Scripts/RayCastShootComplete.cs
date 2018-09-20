41ac036f685490871acf144f1155cb32440fac03
fernando <ejemplo696@gmail.com>
2018-09-20 14:21:03 +0200

    Donus de color

--- Assets/Scripts/RayCastShootComplete.cs
+++ Assets/Scripts/RayCastShootComplete.cs
@@ -21,6 +21,7 @@ public class RayCastShootComplete : MonoBehaviour {
 		// Check if the player has pressed the fire button and if enough time has elapsed since they last fired
 		if (Input.GetButtonDown("Fire1") && Time.time > nextFire) 
 		{
+			string colorUsar = numToString(ColorActivo);
 			// Update the time when our player can fire next
 			nextFire = Time.time + fireRate;
 
@@ -31,31 +32,54 @@ public class RayCastShootComplete : MonoBehaviour {
             Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
 
             // Declare a raycast hit to store information about what our raycast has hit
-            RaycastHit hit;
+            //RaycastHit hit;
 
 			// Set the start position for our visual effect for our laser to the position of gunEnd
 			laserLine.SetPosition (0, gunEnd.position);
 
-			// Check if our raycast has hit anything
-			if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+			RaycastHit[] hits;
+        	hits = Physics.RaycastAll(rayOrigin, fpsCam.transform.forward, weaponRange);
+
+			if(hits.Length > 0)
 			{
-				// Set the end position for our laser line 
-				laserLine.SetPosition (1, hit.point);
-
-				// Get a reference to a health script attached to the collider we hit
-				ShootableBox health = hit.collider.GetComponent<ShootableBox>();
-
-				// If there was a health script attached
-				if (health != null)
+				for (int i = hits.Length-1; i > -1; i--)
 				{
-					// Call the damage function of that script, passing in our gunDamage variable
-					health.Damage (numToString(ColorActivo));
+					Debug.Log(hits[i].collider);
+					RaycastHit hit = hits[i];
+					ShootableBox sB = hit.collider.GetComponent<ShootableBox>();
+
+					if (sB)
+					{
+						laserLine.SetPosition (1, hit.point);
+
+						sB.Damage (colorUsar);
+						return;
+					}
+
+					DonutDeColor dC = hit.collider.GetComponent<DonutDeColor>();
+					if (dC)
+					{
+						if ((colorUsar == "Amarillo" && dC.Color == "Azul") || (colorUsar == "Azul" && dC.Color == "Amarillo")){
+							colorUsar = "Verde";
+						}
+						else if ((colorUsar == "Rojo" && dC.Color == "Azul") || (colorUsar == "Azul" && dC.Color == "Rojo")){
+							colorUsar = "Morado";
+						}
+						else if ((colorUsar == "Amarillo" && dC.Color == "Rojo") || (colorUsar == "Rojo" && dC.Color == "Amarillo")){
+							colorUsar = "Naranja";
+						}
+						Debug.Log(colorUsar);
+						laserLine.SetPosition (1, hit.point);
+					}
+					else
+					{
+						laserLine.SetPosition (1, hit.point);
+					}
 				}
 			}
 			else
 			{
-				// If we did not hit anything, set the end of the line to a position directly in front of the camera at the distance of weaponRange
-                laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+				laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
 			}
 		}
 		else if(Input.GetButtonDown("Fire2")){
@@ -70,7 +94,7 @@ public class RayCastShootComplete : MonoBehaviour {
 		else if (i == 1)
 			return "Azul";
 		else{
-			return "Verde";
+			return "Amarillo";
 		}
 	}
 
--- Assets/Scripts/ShootableBox.cs
+++ Assets/Scripts/ShootableBox.cs
@@ -5,13 +5,16 @@ using System.Collections;
 public class ShootableBox : MonoBehaviour {
 
 	//The box's current health point total
-	public int currentHealth = 3;
-	public string Color;
+	//public int currentHealth = 3;
+	//public string Color;
 
 	[Header("Numerp de animaciones")]
 	public int Rojo;
 	public int Azul;
+	public int Amarillo;
 	public int Verde;
+	public int Naranja;
+	public int Morado;
 	
 	[Header("Referencias")]
 	[SerializeField]
@@ -19,6 +22,7 @@ public class ShootableBox : MonoBehaviour {
 
 	public void Damage(string s)
 	{
+		Debug.Log("Disparo " + s);
 		if(!anim.isPlaying){
 			if (s == "Rojo")
 			{
@@ -30,12 +34,26 @@ public class ShootableBox : MonoBehaviour {
 				int n = Random.Range(1, Azul + 1);
 				anim.Play("Azul" + n.ToString());
 			}
+			else if (s == "Amarillo")
+			{
+				int n = Random.Range(1, Amarillo + 1);
+				anim.Play("Amarillo" + n.ToString());
+			}
 			else if (s == "Verde")
 			{
 				int n = Random.Range(1, Verde + 1);
 				anim.Play("Verde" + n.ToString());
 			}
-		//Debug.Log("Disparo " + s);
+			else if (s == "Naranja")
+			{
+				int n = Random.Range(1, Naranja + 1);
+				anim.Play("Naranja" + n.ToString());
+			}
+			else if (s == "Morado")
+			{
+				int n = Random.Range(1, Morado + 1);
+				anim.Play("Morado" + n.ToString());
+			}
 		}
 	}
 }
